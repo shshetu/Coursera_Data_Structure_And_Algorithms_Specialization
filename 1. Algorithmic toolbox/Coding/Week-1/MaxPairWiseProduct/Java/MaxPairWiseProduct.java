58c97585576c6bb6dac17100bcf8a7e60f0c0c91
shshetu <shshetu2017@gmail.com>
2020-05-21 16:26:12 +0600

    changing folder

---
+++ 1. Algorithmic toolbox/Coding/Week-1/MaxPairWiseProduct/Java/MaxPairWiseProduct.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class MaxPairWiseProduct{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String n = scanner.nextLine();
+        System.out.println(n);
+    }
+}
